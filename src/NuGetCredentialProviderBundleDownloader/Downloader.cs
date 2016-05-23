aedafde67c6d20080b4dd6a5f36c221cdf097148
josemesona <josemesona@hotmail.com>
2016-05-23 16:51:46 -0700

    Initial commit of NuGetCredentialProviderBundleDownloader (#2)
    * Initial commit of NuGetCredentialProviderBundleDownloader

---
+++ src/NuGetCredentialProviderBundleDownloader/Downloader.cs
@@ -0,0 +1,235 @@
+﻿using Microsoft.Build.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace NuGetCredentialProviderBundleDownloader
+{
+    /// <summary>
+    /// A NuGet downloader for the Visual Studio Team Services credential bundle.
+    /// </summary>
+    public sealed class Downloader
+    {
+        /// <summary>
+        /// A default URL to use if one is not specified.  This URL is a VSTS account that will probably always exist and can technically be used by everyone.
+        /// </summary>
+        private static readonly Uri DefaultCredentialBundleUri = new Uri("https://microsoft.pkgs.visualstudio.com/_apis/public/nuget/client/CredentialProviderBundle.zip");
+
+        private readonly IBuildEngine _buildEngine;
+
+        private readonly CancellationToken _cancellationToken;
+
+        public Downloader(IBuildEngine buildEngine, CancellationToken cancellationToken)
+        {
+            _buildEngine = buildEngine;
+            _cancellationToken = cancellationToken;
+        }
+
+        public static bool Execute(string path, string arguments, IBuildEngine buildEngine = null, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            // Parse the arguments as a download URL
+            //
+            Uri downloadUri = DefaultCredentialBundleUri;
+
+            if (!String.IsNullOrWhiteSpace(arguments))
+            {
+                Uri uri;
+
+                if (!Uri.TryCreate(arguments, UriKind.Absolute, out uri) || !Path.GetFileName(uri.LocalPath).Equals("CredentialProviderBundle.zip", StringComparison.OrdinalIgnoreCase))
+                {
+                    throw new ArgumentException(String.Format("The specified NuGet downloader arguments '{0}' are invalid.  The value must be a valid URL that points to CredentialProviderBundle.zip.", arguments));
+                }
+
+                downloadUri = uri;
+            }
+
+            Downloader downloader = new Downloader(buildEngine, cancellationToken);
+
+            FileInfo localFile = new FileInfo(Path.Combine(Path.GetTempPath(), Path.GetFileName(downloadUri.LocalPath)));
+
+            if (downloader.DownloadFile(downloadUri, localFile))
+            {
+                // Only unzip .exe, .dll, and .config files to make it a little faster.  This could cause issues if dependencies are not unzipped...
+                //
+                downloader.Unzip(localFile.FullName, path, patterns: new[] { ".exe", ".dll", ".config" });
+
+                return true;
+            }
+
+            return false;
+        }
+
+        private bool DownloadFile(Uri uri, FileInfo destination)
+        {
+            return Retry(() =>
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    try
+                    {
+                        LogMessage(MessageImportance.Low, "Determining if credential bundle has already been downloaded");
+
+                        if (destination.Exists)
+                        {
+                            // Open the request to get the size of the file
+                            //
+                            webClient.OpenReadTaskAsync(uri).Wait(_cancellationToken);
+
+                            long size = Convert.ToInt64(webClient.ResponseHeaders["Content-Length"]);
+
+                            if (destination.Length == size)
+                            {
+                                LogMessage(MessageImportance.Low, "Credential bundle has already been downloaded");
+                                return true;
+                            }
+                        }
+
+                        // Ensure the directory is created
+                        //
+                        if (!String.IsNullOrWhiteSpace(destination.DirectoryName))
+                        {
+                            Directory.CreateDirectory(destination.DirectoryName);
+                        }
+
+                        LogMessage("Downloading credential bundle from '{0}'", uri);
+
+                        webClient.DownloadFileTaskAsync(uri, destination.FullName).Wait(_cancellationToken);
+
+                        return true;
+                    }
+                    catch (Exception)
+                    {
+                        webClient.CancelAsync();
+
+                        if (destination.Exists)
+                        {
+                            try
+                            {
+                                // Attempt to clean up a partially downloaded file
+                                //
+                                Retry(() => File.Delete(destination.FullName), TimeSpan.FromMilliseconds(200));
+                            }
+                            catch (Exception)
+                            {
+                                // Ignored
+                            }
+                        }
+
+                        return false;
+                    }
+                }
+            }, TimeSpan.FromSeconds(3));
+        }
+
+        private void LogMessage(string format, params object[] args)
+        {
+            LogMessage(MessageImportance.Normal, format, args);
+        }
+
+        private void LogMessage(MessageImportance messageImportance, string format, params object[] args)
+        {
+            if (_buildEngine == null)
+            {
+                Console.WriteLine(format, args);
+            }
+            else
+            {
+                _buildEngine.LogMessageEvent(new BuildMessageEventArgs(format, null, "DownloadNuGet", messageImportance, DateTime.UtcNow, args));
+            }
+        }
+
+        private void Retry(Action action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            Retry<object>(() =>
+            {
+                action();
+                return null;
+            }, retryInterval, retryCount);
+        }
+
+        private T Retry<T>(Func<T> action, TimeSpan retryInterval, int retryCount = 3)
+        {
+            List<Exception> exceptions = new List<Exception>();
+
+            for (int retry = 0; retry < retryCount; retry++)
+            {
+                try
+                {
+                    if (retry > 0)
+                    {
+                        Thread.Sleep(retryInterval);
+                    }
+
+                    return action();
+                }
+                catch (Exception e)
+                {
+                    exceptions.Add(e);
+                }
+            }
+
+            throw new AggregateException(exceptions);
+        }
+
+        private void Unzip(string path, string destination, params string[] patterns)
+        {
+            LogMessage(MessageImportance.Low, "Unzipping credential bundle");
+
+            ICollection<string> unzippedFiles = new List<string>();
+
+            try
+            {
+                using (FileStream fileStream = File.OpenRead(path))
+                {
+                    using (ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Read, leaveOpen: false))
+                    {
+                        foreach (var item in archive.Entries.Where(zipArchiveEntry => patterns.Any(pattern => pattern.Equals("*") || zipArchiveEntry.Name.EndsWith(pattern, StringComparison.OrdinalIgnoreCase))).Select(zipArchiveEntry => new
+                        {
+                            Source = zipArchiveEntry,
+                            Destination = new FileInfo(Path.Combine(destination, zipArchiveEntry.FullName)),
+                        }).TakeWhile(i => !_cancellationToken.IsCancellationRequested))
+                        {
+                            if (!item.Destination.Exists || item.Source.Length != item.Destination.Length)
+                            {
+                                if (!String.IsNullOrWhiteSpace(item.Destination.DirectoryName))
+                                {
+                                    Directory.CreateDirectory(item.Destination.DirectoryName);
+                                }
+
+                                unzippedFiles.Add(item.Destination.FullName);
+
+                                using (Stream readStream = item.Source.Open())
+                                {
+                                    using (Stream writeStream = File.Open(item.Destination.FullName, FileMode.Create, FileAccess.Write, FileShare.None))
+                                    {
+                                        LogMessage(MessageImportance.Low, "Unzipping file '{0}' -> '{1}'", item.Source.FullName, item.Destination.FullName);
+                                        readStream.CopyToAsync(writeStream).Wait(_cancellationToken);
+                                    }
+                                }
+
+                                File.SetLastWriteTime(item.Destination.FullName, item.Source.LastWriteTime.DateTime);
+                            }
+                            else
+                            {
+                                LogMessage(MessageImportance.Low, "File '{0}' is already up-to-date", item.Destination.FullName);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                // Clean up any unzipped files
+                //
+                Parallel.ForEach(unzippedFiles.Where(i => File.Exists(i)), unzippedFile => { Retry(() => File.Delete(unzippedFile), TimeSpan.FromMilliseconds(200)); });
+
+                throw;
+            }
+        }
+    }
+}
---
+++ src/NuGetCredentialProviderBundleDownloader/Properties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("NuGetCredentialProviderBundleDownloader")]
+[assembly: AssemblyDescription("A NuGet downloader that downloads the Visual Studio Team Services credential bundle.")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CBT")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
